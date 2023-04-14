585e01cb0a5c9d3fe203a7679fb7632574ea3b65
zyadfallatah <zyadfallatah@gmail.com>
2023-04-15 00:03:29 +0300

    Base Note Design Completed

--- app/app.js
+++ app/app.js
@@ -1,1 +1,4 @@
+import baseNote from "/components/baseNote/baseNote.js";
 
+for (let i = 0; i < 1; i++)
+  document.querySelector("main").innerHTML += baseNote;
---
+++ components/baseNote/baseNote.js
@@ -0,0 +1,11 @@
+let testVar = `
+<section class="base-note">
+<h3 class="sub-title">sub title</h3>
+<div class="note-info">
+  <h2 class="note-name">Note name</h2>
+  <p class="date">1/1/2000</p>
+</div>
+</section>
+`;
+
+export default testVar;
