0d74ab5ef9723ba3ed48f06c3e5dfc3dfd89e49c
zyadfallatah <zyadfallatah@gmail.com>
2023-04-15 23:51:17 +0300

    baseNote content component added

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 import BaseNote from "/components/baseNote/baseNote.js";
+import BaseNoteContent from "/components/baseNoteContent/baseNoteContent.js";
 
-let myNote = new BaseNote("first note", "today works", "15/4/2023");
+// let myNote = new BaseNote("first hello", "today works", "15/4/2023");
 
-document.querySelector("main").innerHTML = myNote.createBaseNote();
+document.querySelector("main").innerHTML = BaseNoteContent;
---
+++ components/baseNoteContent/baseNoteContent.js
@@ -0,0 +1,17 @@
+let temp = `
+<div class="base-note-content">
+  <nav class="note-info">
+
+    <h2 class="note-name">Note name</h2>
+
+    <div class="sub-info">
+      <h3 class="sub-title">Sub Title</h3>
+      <p class="date">1/1/1990</p>
+    </div>
+
+  </nav>
+  <p class="note-content">Note content</p>
+</div>
+`;
+
+export default temp;
