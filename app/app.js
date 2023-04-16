d41ad912f9d39d25a57b356aad41ab1145b64fb0
zyadfallatah <zyadfallatah@gmail.com>
2023-04-16 17:27:17 +0300

    Creation icon is Added

--- app/app.js
+++ app/app.js
@@ -1,6 +1,36 @@
-import BaseNote from "/components/baseNote/baseNote.js";
 import BaseNoteContent from "/components/baseNoteContent/baseNoteContent.js";
 
-// let myNote = new BaseNote("first hello", "today works", "15/4/2023");
+let myNote = new BaseNoteContent(
+  "first helloaaa9",
+  "today works",
+  "15/4/2023",
+  "Hi every one this is my first note"
+);
 
-document.querySelector("main").innerHTML = BaseNoteContent;
+let addNewNotes = document.querySelector(".add-notes");
+addNewNotes.addEventListener("click", () => {
+  addNewNotes.children[0].classList.toggle("active");
+});
+
+// let main = document.querySelector("main");
+// main.innerHTML = "";
+// main.append(myNote.createBaseNote());
+
+// let getNote = document.querySelector(".base-note");
+
+// getNote.addEventListener("click", function () {
+//   document.body.append(myNote.createBaseNoteContent());
+
+//   let getContentNote = document.querySelector(".base-note-content");
+//   setTimeout(() => {
+//     getContentNote.style.opacity = "1";
+//     getContentNote.style.transform = "translateX(-50%)";
+//   }, 1);
+// });
+
+// document.addEventListener("keydown", function (e) {
+//   if (e.key === "Enter") {
+//     let getContentNote = document.querySelector(".base-note-content");
+//     getContentNote.remove();
+//   }
+// });
