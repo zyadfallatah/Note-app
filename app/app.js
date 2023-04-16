2164d69315eec67b90b6150b1d24fcaadb3ba07f
zyadfallatah <zyadfallatah@gmail.com>
2023-04-17 00:52:21 +0300

    Updated Component {document.createElement()}

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
-import BaseNoteContent from "/components/baseNoteContent/baseNoteContent.js";
+import BaseNoteContent from "../components/baseNoteContent/baseNoteContent.js";
 import BaseNoteCreation from "../components/baseNoteCreate/baseNoteCreate.js";
+
 let myNote = new BaseNoteContent(
   "first helloaaa9",
   "today works",
@@ -7,7 +8,8 @@ let myNote = new BaseNoteContent(
   "Hi every one this is my first note"
 );
 
-// document.body.innerHTML += temp;
+document.body.append(BaseNoteCreation.createBaseNoteCreationPage());
+
 let addNewNotes = document.querySelector(".add-notes");
 addNewNotes.addEventListener("click", () => {
   addNewNotes.children[0].classList.toggle("active");
--- components/baseNoteCreate/baseNoteCreate.js
+++ components/baseNoteCreate/baseNoteCreate.js
@@ -1,43 +1,111 @@
-let temp = `
-      
-<section class="create-base-note">
-  <aside class="options">
-    <h2 class="title">Creation Page</h2>
-    <div class="features">more features will be added</div>
-    <div class="confirm">
-      <input id="cancel" type="button" value="Cancel">
-      <input id="create" type="button" value="Create">
-    </div>
-  </aside>
+class BaseNoteCreation {
+  static createContainer = function (noteInfoType, inputId, limitId, limit) {
+    let input = document.createElement("input");
+    input.type = "text";
+    input.id = inputId;
 
-<div class="reading-data">
-  <div class="header-data">
+    let span = document.createElement("span");
+    span.id = limitId;
+    let spanText = document.createTextNode(limit);
+    span.append(spanText);
 
-    <div class="container">
-      <h3 class="note-name">Note Name</h3>
-      <div class="read-limit">
-        <input type="text" id="read-note-name">
-        <span id="note-name-limit">1</span>
-      </div>
-    </div>
-    <div class="container">
-      <h3 class="note-name">Sub Title</h3>
-      <div class="read-limit">
-        <input type="text" id="read-sub-title">
-        <span id="sub-title-limit">1</span>
-      </div>
-    </div>
+    let readLimit = document.createElement("div");
+    readLimit.className = "read-limit";
+    readLimit.append(input);
+    readLimit.append(span);
 
-  </div>
+    let noteInfo = document.createElement("h3");
+    noteInfo.className = "note-info";
+    let noteInfoText = document.createTextNode(noteInfoType);
+    noteInfo.append(noteInfoText);
 
-  <div class="main-data">
-    <h3 class="text-content">Text Content</h3>
-    <textarea style="resize: none;" id="read-text-content"></textarea>
-  </div>
-</div>
-</section>
+    let container = document.createElement("div");
+    container.className = "container";
+    container.append(noteInfo);
+    container.append(readLimit);
 
+    return container;
+  };
 
-`;
+  static createBaseNoteCreationPage = function () {
+    let cancel = document.createElement("input");
+    cancel.id = "cancel";
+    cancel.type = "button";
+    cancel.value = "Cancel";
 
-export default temp;
+    let create = document.createElement("input");
+    create.id = "create";
+    create.type = "button";
+    create.value = "Create";
+
+    let confirm = document.createElement("div");
+    confirm.className = "confirm";
+    confirm.append(cancel);
+    confirm.append(create);
+
+    let features = document.createElement("div");
+    features.className = "features";
+    let featuresText = document.createTextNode("more features will be added");
+    features.append(featuresText);
+
+    let title = document.createElement("h2");
+    title.className = "title";
+    let titleText = document.createTextNode("Creation Page");
+    title.append(titleText);
+
+    let options = document.createElement("aside");
+    options.className = "options";
+    options.append(title);
+    options.append(features);
+    options.append(confirm);
+
+    let headerData = document.createElement("div");
+    headerData.className = "header-data";
+
+    headerData.append(
+      BaseNoteCreation.createContainer(
+        "Note Name",
+        "read-note-name",
+        "note-name-limit",
+        "1"
+      )
+    );
+
+    headerData.append(
+      BaseNoteCreation.createContainer(
+        "Sub Title",
+        "read-sub-title",
+        "sub-title-limit",
+        "1"
+      )
+    );
+
+    let textContent = document.createElement("h3");
+    textContent.className = "text-content";
+    let textContentText = document.createTextNode("Text Content");
+    textContent.append(textContentText);
+
+    let textArea = document.createElement("textarea");
+    textArea.style.resize = "none";
+    textArea.id = "read-text-content";
+
+    let mainData = document.createElement("div");
+    mainData.className = "main-data";
+    mainData.append(textContent);
+    mainData.append(textArea);
+
+    let readingData = document.createElement("div");
+    readingData.className = "reading-data";
+    readingData.append(headerData);
+    readingData.append(mainData);
+
+    let createBaseNote = document.createElement("section");
+    createBaseNote.className = "create-base-note";
+    createBaseNote.append(options);
+    createBaseNote.append(readingData);
+
+    return createBaseNote;
+  };
+}
+
+export default BaseNoteCreation;
